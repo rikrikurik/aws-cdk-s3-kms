405c2f9d9ee6ec3a725855e771a938e05cd0e6fb
Yoshiyuki Yamariku <55145573+nasubifujii@users.noreply.github.com>
2020-04-23 21:45:19 +0900

    Update cdk context.

--- bin/s3-kms-encryption.ts
+++ bin/s3-kms-encryption.ts
@@ -6,9 +6,10 @@ import { KmsKeyStack } from '../lib/kms-key';
 const app = new cdk.App();
 
 // Get Context
+const name_prefix = app.node.tryGetContext("name")
 const system_name = app.node.tryGetContext("system_name")
 const system_env = app.node.tryGetContext("env")
-const kms_key_name = `${system_name}-${system_env}-key`
+const kms_key_name = `${name_prefix}-${system_name}-${system_env}-key`
 
 // Create stacks
 const kms_key_stack = new KmsKeyStack(app, 'kmskey', kms_key_name, { stackName: kms_key_name });

8f04b1a3072cc1c8d125fd87f08837217fde48ed
Yoshiyuki Yamariku <55145573+nasubifujii@users.noreply.github.com>
2020-04-24 13:26:40 +0900

    Add iam role that granted to access S3 bucket.

--- bin/s3-kms-encryption.ts
+++ bin/s3-kms-encryption.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { KmsKeyStack } from '../lib/kms-key';
 import { S3BucketStack } from '../lib/s3-bucket';
+import { IAMRoleStack } from '../lib/iam-role'
 import { S3DeploymentStack } from '../lib/s3-deployment';
 
 const app = new cdk.App();
@@ -13,6 +14,7 @@ const system_name = app.node.tryGetContext("system_name")
 const system_env = app.node.tryGetContext("env")
 const kms_key_name = `${name_prefix}-${system_name}-${system_env}-key`
 const s3_bucket_name = `${name_prefix}-${system_name}-${system_env}-bucket`.toLowerCase()
+const iam_stack_name = `${name_prefix}-${system_name}-${system_env}-roles`.toLowerCase()
 
 // Define stack env
 const stack_env = {
@@ -20,17 +22,21 @@ const stack_env = {
   region: process.env.CDK_DEFAULT_REGION
 }
 
-
 // Create stacks
-const kms_key_stack = new KmsKeyStack(app, 'kmskey', kms_key_name,
+const kms_stack = new KmsKeyStack(app, 'kmskey', kms_key_name,
   {
     stackName: kms_key_name,
     env: stack_env
   });
-const s3_bucket_stack = new S3BucketStack(app, 's3bucket', s3_bucket_name,
-  kms_key_stack.key,
+const s3_stack = new S3BucketStack(app, 's3bucket', s3_bucket_name,
+kms_stack.key,
   {
     stackName: s3_bucket_name,
+    env: stack_env
+  })
+const role_stack = new IAMRoleStack(app, 'iamrole',
+  {
+    stackName: iam_stack_name,
     env: stack_env
   })
 // const s3_deployment = new S3DeploymentStack(app, 's3deployment',
@@ -41,13 +47,20 @@ const s3_bucket_stack = new S3BucketStack(app, 's3bucket', s3_bucket_name,
 //   })
 
 // Define stack dependencies
-s3_bucket_stack.addDependency(kms_key_stack, 'Using encryption key')
-// s3_deployment.addDependency(s3_bucket_stack, 'Deploy contents into s3 bucket')
+s3_stack.addDependency(kms_stack, 'Using encryption key')
+role_stack.addDependency(kms_stack, 'Using encryption key for s3bucket')
+// s3_deployment.addDependency(kms_stack, 'Deploy contents into s3 bucket')
+
+// attach s3 access policy to iam roles
+s3_stack.bucket.grantReadWrite(role_stack.kms_allowed_role)
+s3_stack.bucket.grantReadWrite(role_stack.kms_denied_role)
 
 // Tagging
-cdk.Tag.add(kms_key_stack, "system", system_name)
-cdk.Tag.add(kms_key_stack, "env", system_env)
-cdk.Tag.add(s3_bucket_stack, "system", system_name)
-cdk.Tag.add(s3_bucket_stack, "env", system_env)
+cdk.Tag.add(kms_stack, "system", system_name)
+cdk.Tag.add(kms_stack, "env", system_env)
+cdk.Tag.add(s3_stack, "system", system_name)
+cdk.Tag.add(s3_stack, "env", system_env)
+cdk.Tag.add(role_stack, "system", system_name)
+cdk.Tag.add(role_stack, "env", system_env)
 // cdk.Tag.add(s3_deployment, "system", system_name)
 // cdk.Tag.add(s3_deployment, "env", system_env)
--- lib/iam-role.ts
+++ lib/iam-role.ts
@@ -4,12 +4,27 @@ import s3 = require('@aws-cdk/aws-s3');
 import kms = require('@aws-cdk/aws-kms');
 
 export class IAMRoleStack extends cdk.Stack {
+  public kms_allowed_role: iam.Role;
+  public kms_denied_role: iam.Role;
 
-  constructor(scope: cdk.Construct, id: string,
-    bucket: s3.Bucket, encryption_key: kms.Key, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Create IAM role
-  }
+    this.kms_allowed_role = new iam.Role(
+      this, 'test role', {
+        assumedBy: new iam.AccountPrincipal(this.account),
+        description: 'IAM Role that allowd to use KMS key'
+      }
+    )
+    this.kms_denied_role = new iam.Role(
+      this, 'test role 2', {
+        assumedBy: new iam.AccountPrincipal(this.account),
+        description: 'IAM Role that prohibited to use KMS key'
+      }
+    )
 
+    // Attach kms key policy to allowed_role
+    
+  }
 }

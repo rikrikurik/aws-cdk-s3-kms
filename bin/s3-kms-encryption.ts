fff4141d8b6663a54f448ef70a99fd6aaabe27aa
Yoshiyuki Yamariku <55145573+nasubifujii@users.noreply.github.com>
2020-04-24 11:37:03 +0900

    Remove s3 test data deployment stack

--- bin/s3-kms-encryption.ts
+++ bin/s3-kms-encryption.ts
@@ -14,22 +14,40 @@ const system_env = app.node.tryGetContext("env")
 const kms_key_name = `${name_prefix}-${system_name}-${system_env}-key`
 const s3_bucket_name = `${name_prefix}-${system_name}-${system_env}-bucket`.toLowerCase()
 
+// Define stack env
+const stack_env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION
+}
+
+
 // Create stacks
 const kms_key_stack = new KmsKeyStack(app, 'kmskey', kms_key_name,
-  { stackName: kms_key_name });
+  {
+    stackName: kms_key_name,
+    env: stack_env
+  });
 const s3_bucket_stack = new S3BucketStack(app, 's3bucket', s3_bucket_name,
   kms_key_stack.key,
-  { stackName: s3_bucket_name })
+  {
+    stackName: s3_bucket_name,
+    env: stack_env
+  })
+// const s3_deployment = new S3DeploymentStack(app, 's3deployment',
+//   s3_bucket_name, kms_key_stack.key,
+//   {
+//     stackName: s3_bucket_name + '-deploy',
+//     env: stack_env
+//   })
 
 // Define stack dependencies
 s3_bucket_stack.addDependency(kms_key_stack, 'Using encryption key')
+// s3_deployment.addDependency(s3_bucket_stack, 'Deploy contents into s3 bucket')
 
 // Tagging
 cdk.Tag.add(kms_key_stack, "system", system_name)
 cdk.Tag.add(kms_key_stack, "env", system_env)
 cdk.Tag.add(s3_bucket_stack, "system", system_name)
 cdk.Tag.add(s3_bucket_stack, "env", system_env)
-
-// Create S3 content deployment stack
-const s3_deployment = new S3DeploymentStack(app, 's3deployment',
-  s3_bucket_name, kms_key_stack.key)
+// cdk.Tag.add(s3_deployment, "system", system_name)
+// cdk.Tag.add(s3_deployment, "env", system_env)
